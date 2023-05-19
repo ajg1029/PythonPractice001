f2a71a1f790f9490c1fde0f735f70262338e6276
ajg1029 <urban.seonbi.deon@gmail.com>
2023-05-19 16:17:10 +0900

    my

--- 230519/test01.py
+++ 230519/test01.py
@@ -6,3 +6,23 @@ for i in range(5):
 
 print('Hello Woldo!')
 
+# myList = [1, 2, 3, 4, 5]
+myDict = {'a': 1, 'b': 2, 'c': 3}
+
+print(myDict.items())
+
+for key, value in myDict.items():
+    print(key, value)
+
+def myFunc():
+    print('Hello World!')
+
+class MyClass:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+    def myFunc(self):
+        print('Hello World!')
+
+print(MyClass().a)
